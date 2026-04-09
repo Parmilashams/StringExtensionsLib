47790c32a8f6092039794566b690cccb14d1ec90
Parmilashams <158627604+Parmilashams@users.noreply.github.com>
2026-04-09 19:16:27 -0400

    Refactor StringExtensions class methods
    Removed unused string extension methods and added a method to check if a string starts with an uppercase letter.

--- Class1.cs
+++ Class1.cs
@@ -2,26 +2,11 @@ namespace StringExtensionsLib
 {
     public static class StringExtensions
     {
-        // Example: Check if string is null or empty
-        public static bool IsNullOrEmpty(this string str)
+        public static bool StartsWithUpperCase(this string str)
         {
-            return string.IsNullOrEmpty(str);
-        }
-
-        // Example: Reverse a string
-        public static string Reverse(this string str)
-        {
-            if (str == null) return null;
-            char[] chars = str.ToCharArray();
-            Array.Reverse(chars);
-            return new string(chars);
-        }
-
-        // Example: Capitalize first letter
-        public static string Capitalize(this string str)
-        {
-            if (string.IsNullOrEmpty(str)) return str;
-            return char.ToUpper(str[0]) + str.Substring(1);
+            if (string.IsNullOrEmpty(str))
+                return false;
+            return char.IsUpper(str[0]);
         }
     }
 }
