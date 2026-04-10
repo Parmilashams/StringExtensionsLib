8be7f4f9448c3929add14cb58ccf7b7c6a6e56b2
Parmila Shams <pshams@my.centennialcollege.ca>
2026-04-09 21:46:34 -0400

    Add StringTestApp console app

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using StringExtensionsLib;
+
+string test1 = "Hello World";
+string test2 = "hello world";
+string test3 = "";
+
+Console.WriteLine($"\"{test1}\" starts with uppercase: {test1.StartsWithUpper()}");
+Console.WriteLine($"\"{test2}\" starts with uppercase: {test2.StartsWithUpper()}");
+Console.WriteLine($"\"{test3}\" starts with uppercase: {test3.StartsWithUpper()}");
